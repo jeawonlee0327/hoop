c06a7696d90928dd5528f6ca6ed494f456a135ee
jeawonlee0327 <dlwpdnjs0327@gmail.com>
2024-02-14 11:57:00 +0900

    base

---
+++ src/main/kotlin/com/teamsparta/hoop/HoopApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.hoop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoopApplication
+
+fun main(args: Array<String>) {
+    runApplication<HoopApplication>(*args)
+}
