babb0216f5d014f802b80a952127d13efe088a5e
홍성욱 <suh75321@naver.com>
2024-02-19 13:14:28 +0900

    짧게 수정

--- src/main/kotlin/com/teamsparta/hoop/service/StoreServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/hoop/service/StoreServiceImpl.kt
@@ -13,9 +13,9 @@ class StoreServiceImpl(
 
     override fun getStoresByTotalEvaluation(rating: Int): List<StoreDto> {
         return storeRepository.findAll()
-            .filter { it.totalEvaluation == rating }
-            .sortedByDescending { it.monitoringDate }
-            .map { StoreDto(it) }
+            .filter { it.totalEvaluation == rating }//가져온 상점 정보 중에서 "totalEvaluation"이 입력받은 "rating"과 같은 상점만 필터링
+            .sortedByDescending { it.monitoringDate }//필터링된 상점들을 "monitoringDate"를 기준으로 내림차순
+            .map { StoreDto(it) }// 정렬된 상점 정보를 StoreDto 객체로 변환하여 리스트로
     }
 
     override fun getStoresBySituation(status: String): List<StoreDto> {
@@ -24,6 +24,20 @@ class StoreServiceImpl(
             .sortedByDescending { it.monitoringDate }
             .map { StoreDto(it) }
     }
+//    .map { store -> StoreDto(
+//        id = store.id,
+//        shopName = store.shopName,
+//        mallName = store.mallName,
+//        domain = store.domain,
+//        email = store.email,
+//        phoneNumber = store.phoneNumber,
+//        businessType = store.businessType,
+//        address = store.address,
+//        totalEvaluation = store.totalEvaluation,
+//        situation = store.situation,
+//        monitoringDate = store.monitoringDate
+//        )} dto에 컨스트럭터로 긴거 안넣으며 여기에 이걸 넣어야 하기 때문
+
 
     override fun getStoresByTotalEvaluationAndSituation(rating: Int, status: String): List<StoreDto> {
         return storeRepository.findAll()
